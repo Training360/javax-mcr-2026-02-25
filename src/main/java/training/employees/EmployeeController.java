0c184c3504c40668503908b0fb52a857d13b2aea
Istvan Viczian <viczian.istvan@gmail.com>
2026-02-25 13:43:28 +0100

    CRUD műveletek funkcionálisan jól működnek

--- src/main/java/training/employees/EmployeeController.java
+++ src/main/java/training/employees/EmployeeController.java
@@ -21,4 +21,24 @@ public class EmployeeController {
     public EmployeeDto joinEmployee(@RequestBody EmployeeDto employee) {
         return employeeService.joinEmployee(employee);
     }
+
+    @GetMapping("/{id}")
+    public EmployeeDto findById(@PathVariable Long id) {
+        return employeeService.findById(id).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public EmployeeDto correct(@PathVariable Long id, @RequestBody EmployeeDto employee) {
+        if (!employee.id().equals(id)) {
+            throw new IllegalArgumentException("Employee id mismatch: %d != %d".formatted(employee.id(), id));
+        }
+        return employeeService.correct(employee);
+    }
+
+    @DeleteMapping("/{id}")
+    public void leave(@PathVariable Long id) {
+        employeeService.leave(id);
+    }
+
+
 }
--- src/main/java/training/employees/EmployeeService.java
+++ src/main/java/training/employees/EmployeeService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -27,6 +28,23 @@ public class EmployeeService {
         EmployeeDto employeeToPersist = new EmployeeDto(idGenerator.incrementAndGet(), employee.name());
         employees.add(employeeToPersist);
         return employeeToPersist;
+    }
+
+    public Optional<EmployeeDto> findById(Long id) {
+        return employees.stream().filter(employee -> employee.id().equals(id)).findFirst();
+    }
+
+    public EmployeeDto correct(EmployeeDto employee) {
+        boolean exists = employees.removeIf(e -> e.id().equals(employee.id()));
+        if (!exists) {
+            throw new IllegalArgumentException("Employee not found: %d".formatted(employee.id()));
+        }
+        employees.add(employee);
+        return employee;
+    }
+
+    public void leave(long id) {
+        employees.removeIf(employee -> employee.id().equals(id));
     }
 
 }
