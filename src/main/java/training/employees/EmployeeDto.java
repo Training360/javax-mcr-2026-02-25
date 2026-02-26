ec11042d200a517ff7805cf50318ddab90017917
Istvan Viczian <viczian.istvan@gmail.com>
2026-02-26 15:37:28 +0100

    Swagger

--- src/main/java/training/employees/EmployeeDto.java
+++ src/main/java/training/employees/EmployeeDto.java
@@ -1,11 +1,14 @@
 package training.employees;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
 public record EmployeeDto(
+        @Schema(description = "az alkalmazott belső azonosítója", example = "1")
         Long id,
 
         @NotEmpty(message = "név nem lehet üres")
         @ValidName(ignored = {"dr.", "ifj."})
+        @Schema(description = "az alkalmazott neve", example = "John Doe")
         String name) {
 }
