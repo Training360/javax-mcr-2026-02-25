3d8689d4473081208e41fa7d755278c3c2051f9c
Istvan Viczian <viczian.istvan@gmail.com>
2026-02-26 10:44:50 +0100

    Spring Data JPA bevezetése

---
+++ src/main/java/training/employees/Employee.java
@@ -0,0 +1,44 @@
+package training.employees;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Employee employee = (Employee) o;
+        return getId() != null && Objects.equals(getId(), employee.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
--- src/main/java/training/employees/EmployeeService.java
+++ src/main/java/training/employees/EmployeeService.java
@@ -1,52 +1,51 @@
 package training.employees;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.io.IOError;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
 
-    private final AtomicLong idGenerator = new AtomicLong();
-
-    private final List<EmployeeDto> employees = new CopyOnWriteArrayList<>(
-            List.of(
-                    new EmployeeDto(idGenerator.incrementAndGet(), "John Doe"),
-                    new EmployeeDto(idGenerator.incrementAndGet(), "Jack Doe"))
-    );
+    private final EmployeeRepository employeeRepository;
 
     public List<EmployeeDto> findAll() {
-        return new ArrayList<>(employees);
+        return employeeRepository
+                .findAll()
+                .stream().map(this::toDto).toList();
     }
 
     public EmployeeDto joinEmployee(EmployeeDto employee) {
-        EmployeeDto employeeToPersist = new EmployeeDto(idGenerator.incrementAndGet(), employee.name());
-        employees.add(employeeToPersist);
-        return employeeToPersist;
+        Employee entity = new Employee(employee.name());
+        Employee saved = employeeRepository.save(entity);
+        return toDto(saved);
+    }
+
+    private EmployeeDto toDto(Employee entity) {
+        return new EmployeeDto(entity.getId(), entity.getName());
     }
 
     public EmployeeDto findById(Long id) {
-        return employees.stream().filter(employee -> employee.id().equals(id)).
-                findFirst()
-                .orElseThrow(() -> new NotFoundException("Employee not found: %d".formatted(id)));
+        return toDto(employeeRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found: %d".formatted(id))));
     }
 
+    @Transactional
     public EmployeeDto correct(EmployeeDto employee) {
-        boolean exists = employees.removeIf(e -> e.id().equals(employee.id()));
-        if (!exists) {
-            throw new NotFoundException("Employee not found: %d".formatted(employee.id()));
-        }
-        employees.add(employee);
-        return employee;
+        Employee entity = employeeRepository
+                .findById(employee.id())
+                .orElseThrow(() -> new NotFoundException("Employee not found: %d".formatted(employee.id())));
+        entity.setName(employee.name());
+        return toDto(entity);
     }
 
     public void leave(long id) {
-        employees.removeIf(employee -> employee.id().equals(id));
+        employeeRepository.deleteById(id);
     }
 
 }
